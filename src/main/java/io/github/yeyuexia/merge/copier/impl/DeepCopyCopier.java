2f599c149614f83726f18e78e6475331c9466602
Liu <zhuo.liu@sap.com>
2018-02-05 16:22:58 +0800

    refine codes

--- src/main/java/io/github/yeyuexia/merge/copier/impl/DeepCopyCopier.java
+++ src/main/java/io/github/yeyuexia/merge/copier/impl/DeepCopyCopier.java
@@ -28,7 +28,7 @@ public class DeepCopyCopier<X, Y> extends Copier<X, Y> {
     @Override
     public Object copy(X from, Y to, Field field, String path) {
         try {
-            return isFinalValue(field) ? PropertyUtils.getNestedProperty(from, field.getName()) : getObjectValue(from, to, field, path);
+            return isFinalValue(field) ? PropertyUtils.getSimpleProperty(from, field.getName()) : getObjectValue(from, to, field, path);
         } catch (InstantiationException | IllegalAccessException e) {
             LOG.error("init field bean error: {}", e);
             throw new MergeException();
@@ -38,13 +38,11 @@ public class DeepCopyCopier<X, Y> extends Copier<X, Y> {
         }
     }
 
-    public static boolean isWrapperType(Class<?> clazz)
-    {
+    public static boolean isWrapperType(Class<?> clazz) {
         return WRAPPER_TYPES.contains(clazz);
     }
 
-    private static Set<Class<?>> getBasicTypes()
-    {
+    private static Set<Class<?>> getBasicTypes() {
         Set<Class<?>> ret = new HashSet<>();
         ret.add(Boolean.class);
         ret.add(Character.class);
@@ -63,15 +61,17 @@ public class DeepCopyCopier<X, Y> extends Copier<X, Y> {
         return field.getType().isEnum() || field.getType().isPrimitive() || isWrapperType(field.getType());
     }
 
-    private Object getObjectValue(X from, Y to, Field field, String path) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException, NoSuchFieldException {
-        Object toValue = PropertyUtils.getNestedProperty(to, field.getName());
-        Object fromValue = PropertyUtils.getNestedProperty(from, field.getName());
+    private Object getObjectValue(X from, Y to, Field field, String path) throws IllegalAccessException,
+            InvocationTargetException, NoSuchMethodException, InstantiationException, NoSuchFieldException {
+        Object toValue = PropertyUtils.getSimpleProperty(to, field.getName());
+        Object fromValue = PropertyUtils.getSimpleProperty(from, field.getName());
         Object fieldBean = toValue == null ? generateInstance(fromValue, field) : toValue;
         merger.merge(fromValue, fieldBean, Helper.getPath(path, field.getName()));
         return fieldBean;
     }
 
-    private Object generateInstance(Object fromValue, Field field) throws NoSuchFieldException, InstantiationException, IllegalAccessException {
+    private Object generateInstance(Object fromValue, Field field) throws NoSuchFieldException,
+            InstantiationException, IllegalAccessException {
         if (field.getType().isInstance(fromValue)) {
             return fromValue.getClass().newInstance();
         } else {

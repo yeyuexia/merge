d4f13570f503409dc0a120869fd4744eac369413
yyx <zhuo.liu1@baozun.com>
2019-04-03 10:59:08 +0800

    fix merge list bugs

--- src/test/java/io/github/yeyuexia/merge/base/data/BaseObject.java
+++ src/test/java/io/github/yeyuexia/merge/base/data/BaseObject.java
@@ -1,5 +1,7 @@
 package io.github.yeyuexia.merge.base.data;
 
+import java.util.Objects;
+
 public class BaseObject {
 
   private String baseObjectStringField;
@@ -19,5 +21,23 @@ public class BaseObject {
 
   public void setBaseObjectIntegerField(Integer baseObjectIntegerField) {
     this.baseObjectIntegerField = baseObjectIntegerField;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BaseObject that = (BaseObject) o;
+    return Objects.equals(baseObjectStringField, that.baseObjectStringField) &&
+        Objects.equals(baseObjectIntegerField, that.baseObjectIntegerField);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(baseObjectStringField, baseObjectIntegerField);
   }
 }
---
+++ src/test/java/io/github/yeyuexia/merge/base/data/ListObject.java
@@ -0,0 +1,15 @@
+package io.github.yeyuexia.merge.base.data;
+
+import java.util.List;
+
+public class ListObject {
+  private List<BaseObject> objects;
+
+  public List<BaseObject> getObjects() {
+    return objects;
+  }
+
+  public void setObjects(List<BaseObject> objects) {
+    this.objects = objects;
+  }
+}
